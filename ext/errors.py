9e578b756602c4c53162c7bd1023be0dee872451
Painezor <Paine.exe@gmail.com>
2020-03-14 10:36:47 +0000

    Fix error on error.

--- ext/errors.py
+++ ext/errors.py
@@ -91,7 +91,7 @@ class Errors(commands.Cog):
             try:
                 await ctx.send('An error occurred, I need embed_links permissions to send embeds.')
             except discord.Forbidden:
-                pass  # well fuck you too then?
+                return  # well fuck you too then?
         except discord.HTTPException:
             print(e.description)
             e.description = "An error occurred, error too long to output in embed."
