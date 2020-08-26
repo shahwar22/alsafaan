89c609fc9c5300431d0ebb815a332d0434409691
Painezor <Paine.exe@gmail.com>
2020-08-26 15:20:15 +0100

    cleanup for new help command.

--- ext/help.py
+++ ext/help.py
@@ -2,9 +2,17 @@ import discord
 from discord.ext import commands
 
 
+def descriptor(command):
+    string = f'• `{command.name}` - {command.short_doc}'
+    if isinstance(command, commands.Group):
+        string += f'\n***Subcommands**:* {", ".join(f"`{sub.name}`" for sub in command.commands)}'
+    return string
+
+
 class Help(commands.HelpCommand):
+    """ The Toonbot help command. """
     def get_command_signature(self, command):
-        return '{0.clean_prefix}{1.qualified_name} {1.signature}'.format(self, command)
+        return f'.tb {command.qualified_name} {command.signature}'
     
     async def send_bot_help(self, mapping):
         """ WIP New Help Command. """
@@ -14,15 +22,22 @@ class Help(commands.HelpCommand):
         e.colour = 0x2ecc71
         e.set_author(name="Toonbot Help")
         
-        e.description = f"Use {self.context.prefix}help **category name** for help on a category\n\n"
+        e.description = f"Use {self.context.prefix}help **category name** for help on a category\n" \
+                        f"*Note: This is case sensitive*\n\n"
         
         cogs = [self.context.bot.get_cog(cog) for cog in self.context.bot.cogs]
         for cog in cogs:
             if not cog.get_commands():
                 continue  # Filter utility cogs.
             
-            if any([i.can_run(self.context) for i in cog.walk_commands()]):
-                e.description += f"**{cog.qualified_name}**: {cog.description}\n"
+            for command in cog.walk_commands():
+                try:
+                    if await command.can_run(self.context):
+                        e.description += f"**{cog.qualified_name}**: {cog.description}\n"
+                        break
+                except discord.ext.commands.CommandError:
+                    pass
+                    
         
         invite_and_stuff = f"[Invite me to your discord]" \
                            f"(https://discordapp.com/oauth2/authorize?client_id=250051254783311873" \
@@ -30,24 +45,21 @@ class Help(commands.HelpCommand):
         invite_and_stuff += f"[Join my Support discord](http://www.discord.gg/a5NHvPx)\n"
         invite_and_stuff += f"[Toonbot on Github](https://github.com/Painezor/Toonbot)"
         e.add_field(name="Useful links", value=invite_and_stuff)
-        
-        await self.context.send(embed=e)
-    
+        try:
+            await self.context.send(embed=e)
+        except discord.Forbidden:
+            await self.context.author.send("I do not have permissions to send help in the channel you requested from.",
+                                           embed=e)
+            
     async def send_cog_help(self, cog):
         await self.context.send(embed=await self.cog_embed(cog))
     
     async def cog_embed(self, cog):
-        def descriptor(command):
-            string = f'`{command.name}` - {command.short_doc}'
-            if isinstance(command, commands.Group):
-                string += f'\n*Subcommands*: ┗ {", ".join(f"`{sub.name}`" for sub in command.commands)}'
-            return string
-        
         e = discord.Embed()
-        e.title = f'{cog.qualified_name} help'
+        e.title = f'{cog.qualified_name} category help'
         e.colour = 0x2ecc71
         e.set_thumbnail(url=self.context.me.avatar_url)
-        e.description = cog.description + "\n"
+        e.description = cog.description + "\n\n"
         e.description += '\n'.join([descriptor(command) for command in cog.get_commands() if not command.hidden])
         e.add_field(name="More help", value='Use help **command** to view the usage of that command.\n'
                                             f'Subcommands are ran by using `{self.context.prefix}command subcommand`')
@@ -55,17 +67,26 @@ class Help(commands.HelpCommand):
     
     async def send_group_help(self, group):
         e = discord.Embed()
-        e.title = f'{group.name} help'
-        e.description = group.help
+        e.title = f'Help for command group: {group.name.title()}'
+        e.description = f"{group.help}\n```{self.get_command_signature(group)}```"
         e.colour = 0x2ecc71
         e.set_thumbnail(url=self.context.me.avatar_url)
         
         if group.aliases:
-            e.add_field(name='Aliases', value=', '.join(group.aliases), inline=False)
+            e.description += '*Command Aliases*: ' + ', '.join([f"`{i}`" for i in group.aliases]) + "\n\n"
+
+        e.description += "***Subcommands**:*: "
         for command in group.commands:
-            e.add_field(name=command.name, value=f'{command.help} ```{self.get_command_signature(command)}```',
+            cmd_string = f"{command.help} ```{self.get_command_signature(command)}```"
+            
+            if isinstance(command, commands.Group):
+                cmd_string += "***Subcommands**:* " + ", ".join([f"`{i.name}`" for i in command.commands])
+            e.add_field(name=command.name, value=cmd_string,
                         inline=False)
-        e.set_footer(text='<REQUIRED argument> | [OPTIONAL argument]')
+            
+        e.add_field(name="More help", value='Use help **command** to view the usage of that command.\n'
+                                            f'Subcommands are ran by using `{self.context.prefix}command subcommand`')
+        e.set_footer(text='<REQUIRED argument> | [OPTIONAL argument] | Use help <command> for further info.')
         await self.context.send(embed=e)
     
     async def send_command_help(self, command):
@@ -82,7 +103,6 @@ class Help(commands.HelpCommand):
         e.set_footer(text='<REQUIRED argument> | [OPTIONAL argument]')
         await self.context.send(embed=e)
     
-    # Override to make cogs not case-sensitive
     async def command_callback(self, ctx, *, command=None):
         await self.prepare_help_command(ctx, command)
         
@@ -90,7 +110,8 @@ class Help(commands.HelpCommand):
             mapping = self.get_bot_mapping()
             return await self.send_bot_help(mapping)
         
-        cog = ctx.bot.get_cog(command.title())
+        cog = ctx.bot.get_cog(command)
+        
         if cog is not None:
             return await self.send_cog_help(cog)
         
@@ -108,7 +129,8 @@ class Help(commands.HelpCommand):
                 return await self.send_error_message(string)
             else:
                 if found is None:
-                    string = await discord.utils.maybe_coroutine(self.subcommand_not_found, cmd, self.remove_mentions(key))
+                    string = await discord.utils.maybe_coroutine(self.subcommand_not_found, cmd,
+                                                                 self.remove_mentions(key))
                     return await self.send_error_message(string)
                 cmd = found
         
