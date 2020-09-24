0ed1d3bf98a14c949c7dcbbf0f8fd92a38775b2f
Painezor <Paine.exe@gmail.com>
2020-09-24 14:57:45 +0100

    use embed_utils for tv searche plus general pep8

--- ext/tv.py
+++ ext/tv.py
@@ -7,94 +7,54 @@ import json
 import aiohttp
 from lxml import html
 
+from ext.utils import embed_utils
+
+
 class Tv(commands.Cog):
 	""" Search for live TV matches """
+	
 	def __init__(self, bot):
 		self.bot = bot
 		with open('tv.json') as f:
 			bot.tv = json.load(f)
-			
+	
 	async def save_tv(self):
 		with await self.bot.configlock:
-			with open('tv.json',"w",encoding='utf-8') as f:
-				json.dump(self.bot.tv,f,ensure_ascii=True,
-				sort_keys=True,indent=4, separators=(',',':'))
-
-	@commands.command()
-	@commands.is_owner()
-	async def poptv(self,ctx):
-		""" Repopulate the livescoreTV team Database """
-		with ctx.typing():
-			await ctx.send("Rebuilding the TV Database...")
-			async with self.bot.session.get("http://www.livesoccertv.com/competitions/") as comps:
-				compstree = html.fromstring(await comps.text())
-				complist = compstree.xpath('.//div[@class="tab_container"]//ul/li/a/@href')
-				compname = compstree.xpath('.//div[@class="tab_container"]//ul/li/a//text()')
-				comps = zip(complist,compname)
-				# Phase 1 :  GET ALL COMPETITION TV LISTINGS
-				for i in comps:
-					tvdict.update({i[1]:f"http://www.livesoccertv.com{i[0]}"})
-			# Phase 2 : Get all teams from competitions.
-			for i in {key: value[:] for key, value in tvdict.items()}:
-				async with self.bot.session.get(tvdict[i]) as resp:
-					tree = html.fromstring(await resp.text())
-					# If a table exists, we can grab the teams from it.
-					teams = tree.xpath('.//table//table//tr//td/a')
-					for i in teams:
-						y = "".join(i.xpath('.//text()'))
-						z = "".join(i.xpath('.//@href'))
-						if y and z:
-							self.bot.tv.update({y:f"http://www.livesoccertv.com{z}"})
-			await ctx.send("Done. Saving")
-			await self.save_tv()
-			await ctx.send("Saved.")
-
-	async def _pick_team(self,ctx,team):
+			with open('tv.json', "w", encoding='utf-8') as f:
+				json.dump(self.bot.tv, f, ensure_ascii=True, sort_keys=True, indent=4, separators=(',', ':'))
+				
+	async def _pick_team(self, ctx, team):
 		em = discord.Embed()
-		em.color = 0x034f76
-		em.set_author(name = "LiveSoccerTV.com")
-
-		if team is not None:
-			matches = {i for i in self.bot.tv if team.lower() in i.lower()}
-			if not matches:
-				return await ctx.send(f"Could not find a matching team/league for {team}.")
-			if len(matches) == 1:
-				team = list(matches)[0]
-			else:
-				for i in matches:
-					if team == i:
-						result = i
-				else:
-					count = 0
-					matchdict = {}
-					for i in matches:
-						matchdict[str(count)] = i
-						count += 1
-					matchlist = [f'{i}: {matchdict[i]}' for i in matchdict]
-					strify = '\n'.join(matchlist)
-					msg = f"Please type matching id ```{strify}```"
-					def check(message):
-						if message.author.id == ctx.author.id and message.content in matchdict:
-							return True
-					delme = await ctx.send(msg)
-					result = await self.bot.wait_for("message",check=check,timeout=30)
-					team = matchdict[result.content]
-					await delme.delete()
-					await result.delete()
-			
-			em.url = self.bot.tv[team]
-			em.title = f"Televised Fixtures for {team}\n"
-		else:
-			em.url = "http://www.livesoccertv.com/schedules/"
-			em.title = f"Today's Televised Matches\n"
+		em.colour = 0x034f76
+		em.set_author(name="LiveSoccerTV.com")
 		em.description = ""
+		
+		if not team:
+			em.url = "http://www.livesoccertv.com/schedules/"
+			em.title = f"Today's Televised Matches"
+		
+		item_list = [i for i in self.bot.tv if team.lower() in i.lower()]
+		if not item_list:
+			await ctx.send(f"Could not find a matching team/league for {team}.")
+			return None
+		
+		matching_teams = {i for i in self.bot.tv if team.lower() in i.lower()}
+		
+		index = await embed_utils.page_selector(ctx, matching_teams)
+		team = matching_teams[index]
+		em.url = self.bot.tv[team]
+		em.title = f"Televised Fixtures for {team}"
 		return em
-
+	
 	@commands.command()
-	async def tv(self,ctx,*,team:commands.clean_content = None):
+	async def tv(self, ctx, *, team: commands.clean_content = None):
 		""" Lookup next televised games for a team """
 		async with ctx.typing():
-			em = await self._pick_team(ctx,team)
+			em = await self._pick_team(ctx, team)
+			
+			if em is None:
+				return
+			
 			tvlist = []
 			async with self.bot.session.get(em.url) as resp:
 				if resp.status != 200:
@@ -106,9 +66,9 @@ class Tv(commands.Cog):
 				for i in tree.xpath(".//table[@class='schedules'][1]//tr"):
 					# Discard finished games.
 					isdone = "".join(i.xpath('.//td[@class="livecell"]//span/@class')).strip()
-					if isdone in ["narrow ft","narrow repeat"]:
+					if isdone in ["narrow ft", "narrow repeat"]:
 						continue
-						
+					
 					match = "".join(i.xpath(f'.//td[{matchcol}]//text()')).strip()
 					if not match:
 						continue
@@ -122,17 +82,17 @@ class Tv(commands.Cog):
 					
 					ml = ", ".join([x.strip() for x in ml if x != "nufcTV" and x.strip() != ""])
 					
-					if ml == []:
+					if not ml:
 						continue
-
+					
 					date = "".join(i.xpath('.//td[@class="datecell"]//span/text()')).strip()
 					time = "".join(i.xpath('.//td[@class="timecell"]//span/text()')).strip()
-
+					
 					if isdone != "narrow live":
 						# Correct TimeZone offset.
 						try:
-							time = datetime.datetime.strptime(time,'%H:%M')+ datetime.timedelta(hours=5)
-							time = datetime.datetime.strftime(time,'%H:%M')
+							time = datetime.datetime.strptime(time, '%H:%M') + datetime.timedelta(hours=5)
+							time = datetime.datetime.strftime(time, '%H:%M')
 							dt = f"{date} {time}"
 						except ValueError as e:
 							dt = ""
@@ -140,7 +100,7 @@ class Tv(commands.Cog):
 						dt = i.xpath('.//td[@class="timecell"]//span/text()')[-1].strip()
 						if dt == "FT":
 							continue
-						if dt != "HT" and not ":" in dt:
+						if dt != "HT" and ":" not in dt:
 							dt = f"LIVE {dt}'"
 					else:
 						if date == datetime.datetime.now().strftime("%b %d"):
@@ -148,12 +108,12 @@ class Tv(commands.Cog):
 						else:
 							dt = date
 					
-					tvlist.append((f'`{dt}` [{match}]({link})'))
+					tvlist.append(f'`{dt}` [{match}]({link})')
 			
 			if not tvlist:
 				return await ctx.send(f"Couldn't find any televised matches happening soon, check online at {em.url}")
 			dtn = datetime.datetime.now().strftime("%H:%M")
-
+			
 			em.set_footer(text=f"Time now: {dtn} Your Time:")
 			em.timestamp = datetime.datetime.now()
 			chars = 0
@@ -163,7 +123,7 @@ class Tv(commands.Cog):
 					em.description += x + "\n"
 					remain -= 1
 				chars += len(x) + 5
-				
+			
 			if remain:
 				em.description += f"\n *and {remain} more...*"
 			await ctx.send(embed=em)
